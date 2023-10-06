6cb8a7bf39006dd357bd955234c55dc0260571ff
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-06 10:22:00 -0300

    create ft_strlcpy file

---
+++ ft_strlcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 10:08:47 by pdrago            #+#    #+#             */
+/*   Updated: 2023/10/06 10:10:16 by pdrago           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Copies up to size - 1 characters from the NUL-terminated source 
+string to a destine string, NUL-terminating the result.
+*/
+
+size_t	strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	len;
+
+	return (len);
+}
