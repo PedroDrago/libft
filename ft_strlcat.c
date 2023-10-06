69df3c1a96957f2916cf5f0ce74e86831a89073f
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-06 10:21:47 -0300

    create ft_strlcat file

---
+++ ft_strlcat.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 10:10:22 by pdrago            #+#    #+#             */
+/*   Updated: 2023/10/06 10:11:18 by pdrago           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Appends at most size - ft_strlen(destine - 1) bytes from a
+NUL-terminated source string to the end of a destine string.
+*/
+
+size_t	strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len;
+
+
+	return (len);
+}
