78b10fb29fa8cdcaa2d22b4b283c93416571017e
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-12 14:20:10 -0300

    implement ft_sltnew

---
+++ ft_lstnew.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 14:00:58 by pdrago            #+#    #+#             */
+/*   Updated: 2023/10/12 14:17:34 by pdrago           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Allocates (with malloc(3)) and returns a new node.
+The member variable ’content’ is initialized with
+the value of the parameter ’content’. The variable
+’next’ is initialized to NULL.
+*/
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *) malloc (sizeof(content) + sizeof (t_list *));
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
