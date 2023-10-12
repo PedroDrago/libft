ed95729d1e9328553816506b406bbeaaae74807a
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-12 07:45:11 -0300

    implement ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 10:08:47 by pdrago            #+#    #+#             */
-/*   Updated: 2023/10/06 23:23:11 by pdrago           ###   ########.fr       */
+/*   Updated: 2023/10/07 03:51:04 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,18 @@ Copies up to size - 1 characters from the NUL-terminated source
 string to a destine string, NUL-terminating the result.
 */
 
-size_t	strlcpy(char *dst, const char *src, size_t size);
+size_t	strlcpy(char *dst, const char *src, size_t size)
+{
+	int	count;
+
+	count = 0;
+	while (src[count] && count < size)
+	{
+		dst[count] = src[count];
+		count++;
+	}
+
+	if (size > 0)
+		dst[count] = '\0';
+	return (count);
+}
