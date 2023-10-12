26acbd4655ee9cefb48dfb4d2d3a389e986edec3
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-12 10:25:59 -0300

    create ft_memmove file

---
+++ ft_memmove.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 08:39:29 by pdrago            #+#    #+#             */
+/*   Updated: 2023/10/12 09:14:46 by pdrago           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Copies n bytes from a source memory area to a destine 
+memory area. The areas may overlap.
+*/
+
+void *memmove(void *dest, const void *src, size_t n);
