716b3ed812adc703e52608744697c6da08239ec8
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-12 11:17:43 -0300

    erase itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 02:34:14 by pdrago            #+#    #+#             */
-/*   Updated: 2023/10/07 02:13:14 by pdrago           ###   ########.fr       */
+/*   Updated: 2023/10/12 11:10:50 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,63 +19,4 @@ representing the integer received as an argument.
 Negative numbers must be handled.
 */
 
-static int	count_digits(int n)
-{
-	int	digits;
-
-	digits = 0;
-	while (n != 0)
-	{
-		n /= 10;
-		++digits;
-	}
-	return (digits);
-}
-
-char	*allocate_string(int *n, int *digits)
-{
-	char	*str;
-
-	if ((*n) < 0)
-	{
-		str = (char *) malloc (sizeof(char) * ((*digits) + 2));
-		if (!str)
-			return (NULL);
-		str[0] = '-';
-		*n = -(*n);
-		(*digits)++;
-	}
-	else
-	{
-		str = (char *) malloc (sizeof(char) * ((*digits) + 1));
-		if (!str)
-			return (NULL);
-	}
-	return (str);
-}
-
-char	*ft_itoa(int n)
-{
-	char	*str;
-	int		digits;
-
-	if (n == 0)
-	{
-		str = (char *) malloc (sizeof(char) * 2);
-		if (!str)
-			return (NULL);
-		str[0] = '0';
-		str[1] = 0;
-		return (str);
-	}
-	digits = count_digits(n);
-	str = allocate_string(&n, &digits);
-	while (n != 0)
-	{
-		str[digits - 1] = (n % 10) + '0';
-		digits--;
-		n = n / 10;
-	}
-	str[digits - 2] = '\0';
-	return (str);
-}
+char	*ft_itoa(int n);
