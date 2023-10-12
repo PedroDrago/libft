1aec2397a8315f17c60b7c7ec23410f7d28d6f43
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-12 13:59:33 -0300

    deletes memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 08:39:29 by pdrago            #+#    #+#             */
-/*   Updated: 2023/10/12 09:14:46 by pdrago           ###   ########.fr       */
+/*   Updated: 2023/10/12 13:54:31 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,4 @@ Copies n bytes from a source memory area to a destine
 memory area. The areas may overlap.
 */
 
-void *memmove(void *dest, const void *src, size_t n);
+void	*memmove(void *dest, const void *src, size_t n);
