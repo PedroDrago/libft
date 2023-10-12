38df4547f6e2f3c140beb52958c0bf0a6fbaceab
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-12 13:59:54 -0300

    uncomment bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 17:53:29 by pdrago            #+#    #+#             */
-/*   Updated: 2023/10/12 08:41:19 by pdrago           ###   ########.fr       */
+/*   Updated: 2023/10/12 13:54:53 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,8 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
-void 	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
@@ -53,7 +52,7 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 // Bonus
-/*
+
 typedef struct s_list
 {
 	void	*content;
@@ -61,13 +60,13 @@ typedef struct s_list
 } t_list;
 
 t_list *ft_lstnew(void *content);
-//void ft_lstadd_front(t_list **lst, t_list *new);
+void ft_lstadd_front(t_list **lst, t_list *new);
 int ft_lstsize(t_list *lst);
 t_list *ft_lstlast(t_list *lst);
-//void ft_lstadd_back(t_list **lst, t_list *new);
+void ft_lstadd_back(t_list **lst, t_list *new);
 void ft_lstdelone(t_list *lst, void (*del)(void*));
 void ft_lstclear(t_list **lst, void (*del)(void*));
 void ft_lstiter(t_list *lst, void (*f)(void *));
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *));
-*/
+
 #endif
