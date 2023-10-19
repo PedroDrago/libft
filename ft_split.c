f6e2766aef3f87ca75ea69d82d5382188b5ead9b
pdrago <jupurity6@gmail.com>
2023-10-19 15:06:31 -0300

    lets go

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 01:58:34 by pdrago            #+#    #+#             */
-/*   Updated: 2023/10/19 13:20:10 by pdrago           ###   ########.fr       */
+/*   Updated: 2023/10/19 14:37:15 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,84 +19,88 @@ character ’c’ as a delimiter. The array must end
 with a NULL pointer
 */
 
-static int	ft_wordcount(char const *str, char sep)
+static int	ft_split_count(char const *str, char sep)
 {
-	int	i;
-	int	count;
+	int	index;
+	int	splits;
 
 	if (str == 0 || str[0] == 0)
 		return (0);
-	i = 1;
-	count = 0;
+	index = 1;
+	splits = 0;
 	if (str[0] != sep)
-		count++;
-	while (str[i] != '\0')
+		splits++;
+	while (str[index] != '\0')
 	{
-		if (str[i] != sep && str[i - 1] == sep)
-			count++;
-		i++;
+		if (str[index] != sep && str[index - 1] == sep)
+			splits++;
+		index++;
+	}
+	return (splits);
+}
+
+static int	ft_next_split_count(char const *str, char sep, int index)
+{
+	int	count;
+
+	count = 0;
+	while (str[index] == sep && str[index] != '\0')
+		index++;
+	while (str[index] != '\0' && str[index] != sep)
+	{
+		count++;
+		index++;
 	}
 	return (count);
 }
 
-static char	*ft_new_str_dup(char const *s, int start, int end)
+static char	**ft_free_all(char **str_tab, int max)
 {
-	char	*new_str;
-	int		count;
+	int	index;
 
-	if (start >= end)
-		return (NULL);
-	new_str = (char *) malloc (sizeof(char) * end - start + 1);
-	if (!new_str)
-		return (NULL);
-	count = 0;
-	while (start < end)
+	index = 0;
+	while (index < max && str_tab[index] != 0)
 	{
-		new_str[count] = s[start];
-		count++;
-		start++;
+		free(str_tab[index]);
+		index++;
 	}
-	new_str[count] = '\0';
-	return (new_str);
+	free(str_tab);
+	return (0);
 }
 
-static char	**split_split(char *s, char c, char **sp)
+char	**split_split(char const *s, char c, char **tab_str)
 {
-	int	c1;
-	int	count2;
-	int	start;
-	int	end;
+	int		char_s;
+	int		tab_count;
+	int		char_tab;
 
-	start = 0;
-	end = -1;
-	count2 = 0;
-	c1 = -1;
-	while (s[++c1])
+	char_s = 0;
+	tab_count = -1;
+	while (++tab_count < ft_split_count(s, c))
 	{
-		if (s[c1 - 1] == c && s[c1] != c)
-			start = c1;
-		if ((s[c1 + 1] == c && s[c1] != c) || (s[c1 + 1] == '\0'))
-			end = c1 + 1;
-		if (start >= 0 && end >= 0)
-		{
-			sp[count2] = ft_new_str_dup(s, start, end);
-			count2++;
-			start = -1;
-			end = -1;
-		}
+		char_tab = 0;
+		tab_str[tab_count] = malloc(ft_next_split_count(s, c, char_s) + 1);
+		if (!(tab_str[tab_count]))
+			return (ft_free_all(tab_str, tab_count));
+		while (s[char_s] != '\0' && s[char_s] == c)
+			char_s++;
+		while (s[char_s] != '\0' && s[char_s] != c)
+			tab_str[tab_count][char_tab++] = s[char_s++];
+		tab_str[tab_count][char_tab] = '\0';
 	}
-	sp[count2] = NULL;
-	return (sp);
+	tab_str[tab_count] = 0;
+	return (tab_str);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**splitted;
+	char	**tab_str;
 
 	if (s == 0)
 		return (0);
-	splitted = (char **) malloc (sizeof(char *) * (ft_wordcount(s, c) + 1));
-	if (!splitted)
-		return (NULL);
-	return (split_split(ft_strtrim(s, &c), c, splitted));
+	tab_str = malloc(sizeof(*tab_str) * (ft_split_count(s, c) + 1));
+	if (!(tab_str))
+		return (0);
+	tab_str = split_split(s, c, tab_str);
+	return (tab_str);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 10:17:00 by pdrago            #+#    #+#             */
-/*   Updated: 2023/10/18 23:35:20 by pdrago           ###   ########.fr       */
+/*   Updated: 2023/10/19 14:46:26 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,13 @@ more than len characters are searched.  Characters that appear
 after a ‘\0’ character are not searched.
 */
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+static char	*ft_search(const char *big, const char *little,
+	size_t len, size_t little_len)
 {
 	size_t	count;
 	char	*little_start;
-	size_t	little_len;
 
 	count = 0;
-	little_len = ft_strlen(little);
-	if (!*little)
-		return ((char *) big);
 	while (*big && len--)
 	{
 		if (*big == little[count])
@@ -46,4 +43,16 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 		big++;
 	}
 	return (NULL);
+}
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	little_len;
+
+	if (!*little)
+		return ((char *) big);
+	little_len = ft_strlen(little);
+	if (len == 0)
+		return (NULL);
+	return (ft_search(big, little, len, little_len));
 }
