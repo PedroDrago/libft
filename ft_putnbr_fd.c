e4cf9dc5c37bf2f621766d3c05c09dfb7ef1d5a1
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-11-08 18:12:31 -0300

    refactor int_min validations

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 00:41:01 by pdrago            #+#    #+#             */
-/*   Updated: 2023/10/06 00:47:36 by pdrago           ###   ########.fr       */
+/*   Updated: 2023/11/08 18:11:51 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,19 @@ descriptor
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
+	long nb;
+
+	nb = n;
+	if (nb < 0)
 	{
-		write(fd, "-2", 2);
-		ft_putnbr_fd(147483648, fd);
+		ft_putchar_fd('-', fd);
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
 	}
 	else
-	{
-		if (n < 0)
-		{
-			ft_putchar_fd('-', fd);
-			n = -n;
-		}
-		if (n > 9)
-		{
-			ft_putnbr_fd(n / 10, fd);
-			ft_putnbr_fd(n % 10, fd);
-		}
-		else
-		{
-			ft_putchar_fd((n + '0'), fd);
-		}
-	}
+		ft_putchar_fd((nb + '0'), fd);
 }
